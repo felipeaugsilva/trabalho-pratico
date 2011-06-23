2977a23c5816b165249bd469697c17a6d62533a2
felipe.aug.silva@gmail.com <felipe.aug.silva@gmail.com@40d3e49d-451d-1dd5-ae6e-83f8caf0a0cb>
2011-06-23 22:43:53 +0000

    Heuristica gulosa.

--- main.c
+++ main.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 typedef enum boolean { false, true } boolean;
 
-void alocaVetor( int** vetor, int tamanho)
+typedef struct shard {
+    int i, j;
+    float ganhoPorCusto;
+    char sat;
+} shard;
+
+void alocaVetor( int** vetor, int numSats)
 {
-    *vetor = (int*) malloc( tamanho * sizeof(int) );
+    *vetor = (int*) malloc( numSats * sizeof(int) );
 }
 
-void alocaMatrizInt( int*** matriz, int tamanho )
+void alocaMatrizInt( int*** matriz, int numSats )
 {
     int i;
     
-    *matriz = (int**) malloc( tamanho * sizeof(int*) );
-    for( i=0; i<tamanho; i++)
-        (*matriz)[i] = (int*) malloc( tamanho * sizeof(int) );
+    *matriz = (int**) malloc( numSats * sizeof(int*) );
+    for( i=0; i<numSats; i++)
+        (*matriz)[i] = (int*) malloc( numSats * sizeof(int) );
 }
 
-void alocaMatrizChar( char*** matriz, int tamanho )
+void alocaMatrizChar( char*** matriz, int numSats )
 {
     int i;
     
-    *matriz = (char**) malloc( tamanho * sizeof(char*) );
-    for( i=0; i<tamanho; i++)
-        (*matriz)[i] = (char*) malloc( tamanho * sizeof(char) );
+    *matriz = (char**) malloc( numSats * sizeof(char*) );
+    for( i=0; i<numSats; i++)
+        (*matriz)[i] = (char*) malloc( numSats * sizeof(char) );
 }
 
 void leSatelites( int* S, int numSats, FILE* dados )
@@ -38,13 +45,13 @@ void leSatelites( int* S, int numSats, FILE* dados )
     }
 }
 
-void inicializaMatrizes( int** ganhos, int** custosH, int** custosV, char** shards, int tamanho )
+void inicializaMatrizes( int** ganhos, int** custosH, int** custosV, char** shards, int numSats )
 {
     int i, j;
     
-    for( i=0; i<tamanho; i++)
+    for( i=0; i<numSats; i++)
     {
-        for( j=0; j<tamanho; j++)
+        for( j=0; j<numSats; j++)
         {
             ganhos[i][j] = 0;
             custosH[i][j] = custosV[i][j] = -1;
@@ -53,7 +60,7 @@ void inicializaMatrizes( int** ganhos, int** custosH, int** custosV, char** shar
     }
 }
 
-void leShards( int** ganhos, int** custosH, int** custosV, int tamanho, int numShards, FILE* dados )
+void leShards( int** ganhos, int** custosH, int** custosV, int numSats, int numShards, FILE* dados )
 {
     int i, j, k;
     int ganho, custoH, custoV;
@@ -65,6 +72,120 @@ void leShards( int** ganhos, int** custosH, int** custosV, int tamanho, int numS
         custosH[i-1][j-1] = custoH;
         custosV[i-1][j-1] = custoV;
     }
+}
+
+int comparaGanhoPorCusto( const void* a, const void* b )
+{
+    shard** shardAptr = (shard**) a;
+    shard** shardBptr = (shard**) b;
+    
+    shard* shardA = *shardAptr;
+    shard* shardB = *shardBptr;
+    
+    if( shardA->ganhoPorCusto > shardB->ganhoPorCusto )
+        return -1;
+    
+    return ( shardA->ganhoPorCusto < shardB->ganhoPorCusto );
+}
+
+void constroiSolucao( int* Sh, int* Sv, int** ganhos, int** custosH, int** custosV, char** shards, int numSats, int numShards )
+{
+    shard** vetorShards;
+    int* capacH;
+    int* capacV;
+    int i, j, k;
+    
+    alocaVetor( &capacH, numSats );
+    alocaVetor( &capacV, numSats );
+    
+    memcpy( capacH, Sh, numSats * sizeof(int) );
+    memcpy( capacV, Sv, numSats * sizeof(int) );
+    
+    vetorShards = (shard**) malloc( 2 * numShards * sizeof(shard*) );
+    for( k=0; k<2*numShards; k++ )
+        vetorShards[k] = (shard*) malloc( sizeof(shard) );
+    
+    k = 0;
+    for( i=0; i<numSats; i++ )
+    {
+        for( j=0; j<numSats; j++ )
+        {
+            if( ganhos[i][j] != 0 )
+            {
+                vetorShards[k]->i = i;
+                vetorShards[k]->j = j;
+                vetorShards[k]->sat = 'h';
+                vetorShards[k]->ganhoPorCusto = (float) ganhos[i][j] / custosH [i][j];
+                k++;
+                
+                vetorShards[k]->i = i;
+                vetorShards[k]->j = j;
+                vetorShards[k]->sat = 'v';
+                vetorShards[k]->ganhoPorCusto = (float) ganhos[i][j] / custosV [i][j];
+                k++;
+            }
+        }
+    }
+    
+    qsort( vetorShards, 2*numShards, sizeof(shard*), comparaGanhoPorCusto );
+    
+    for( k=0; k<2*numShards; k++ )
+    {
+        i = vetorShards[k]->i;
+        j = vetorShards[k]->j;
+        
+        if( shards[i][j] == 'n' )
+        {
+            if( vetorShards[k]->sat == 'h' && custosH[i][j] <= capacH[i] )
+            {
+                shards[i][j] = 'h';
+                capacH[i] -= custosH[i][j];
+            }
+            else if ( custosV[i][j] <= capacV[i] )
+            {
+                shards[i][j] = 'v';
+                capacV[i] -= custosV[i][j];
+            }
+        }
+    }
+    
+    free( capacH );
+    free( capacV );
+}
+
+int calculaObjetivo( int** ganhos, char** shards, int numSats )
+{
+    int obj = 0;
+    int i, j;
+    char sat;
+    
+    for( i=0; i<numSats; i++)
+    {
+        for( j=0; j<numSats; j++ )
+        {
+            sat = shards[i][j];
+            if( sat != 'n' )
+            {
+                obj += ganhos[i][j];
+                //printf( "%3d %3d %c\n", i+1, j+1, sat );  // imprimir no arquivo de saida
+            }
+        }
+    }
+    
+    return obj;
+}
+
+int calculaOtimo( int** ganhos, int numSats )
+{
+    int otimo = 0;
+    int i, j;
+    
+    for( i=0; i<numSats; i++)
+        for( j=0; j<numSats; j++ )
+            if( ganhos[i][j] > 0 )
+                otimo += ganhos[i][j];
+    
+    return otimo;
 }
 
 int main( int argc, char *argv[] )
@@ -79,6 +200,7 @@ int main( int argc, char *argv[] )
     int** custosH;
     int** custosV;
     char** shards;
+    int obj;
     
     fscanf( dados, "%d", &numSats );
     
@@ -98,6 +220,19 @@ int main( int argc, char *argv[] )
     inicializaMatrizes( ganhos, custosH, custosV, shards, numSats );
     
     leShards( ganhos, custosH, custosV, numSats, numShards, dados );
+    
+    fclose( dados );
+    
+    constroiSolucao( Sh, Sv, ganhos, custosH, custosV, shards, numSats, numShards );
+    
+    obj = calculaObjetivo( ganhos, shards, numSats );
+    
+    printf( "\nFuncao Objetivo: %d\n\n", obj );
+    
+    /* Usado apenas pra calcular porcentagem em relacao ao valor "otimo" */
+    int otimo = calculaOtimo( ganhos, numSats );
+    printf( "Valor Otimo: %d\n\n", otimo );
+    printf( "Porcentagem: %f\n\n", ((float) obj / otimo) * 100 );
     
     return 0;
 }
